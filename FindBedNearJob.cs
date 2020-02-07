57afd4fd599ca9f499b0264172e97ee8d2984f9a
Gamer_Z <github@gz0.nl>
2020-02-07 16:19:01 +0100

    Initial commit

---
+++ FindBedNearJob.cs
@@ -0,0 +1,16 @@
+﻿using Harmony;
+using static ModLoader;
+
+namespace grasmanek94.FindBedNearJob
+{
+    [ModManager]
+    public static class FindBedNearJob
+    {
+        [ModCallback(EModCallbackType.OnAssemblyLoaded, "grasmanek94.FindBedNearJob.OnAssemblyLoaded")]
+        static void OnAssemblyLoaded(string assemblyPath)
+        {
+            var harmony = HarmonyInstance.Create("grasmanek94.FindBedNearJob");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ FindBedNearJobHooks.cs
@@ -0,0 +1,53 @@
+﻿using BlockEntities.Implementations;
+using Harmony;
+using NPC;
+using Pipliz;
+
+namespace grasmanek94.FindBedNearJob
+{
+	[HarmonyPatch(typeof(NPCBase))]
+	[HarmonyPatch("CalculateGoalLocation")]
+	public class NPCBaseHookNPCGoal
+	{
+		static bool Prefix(NPCBase __instance, ref Vector3Int __result, NPCBase.NPCGoal goal)
+		{
+			if(__instance == null || __instance.Colony == null || __instance.Colony.BedTracker == null)
+			{
+				return true;
+			}
+
+			switch (goal)
+			{
+				case NPCBase.NPCGoal.Bed:
+				{
+					if (__instance.UsedBed != null && __instance.UsedBed.IsValid)
+					{
+						return true;
+					}
+
+					if(__instance.Job == null)
+					{
+						return true;
+					}
+
+					Vector3Int jobLocation;
+					BedTracker.Bed bed;
+					if (__instance.Colony.BedTracker.TryGetClosestUnused(__instance.Job.GetJobLocation(), out jobLocation, out bed, 200))
+					{
+						Log.WriteWarning("Redirected NPC Goal");
+
+						ClassUtility.Call(__instance, "ClearBed", new object[] { });
+						ClassUtility.SetProperty(__instance, "UsedBed", bed);
+						ClassUtility.SetProperty(__instance, "UsedBedLocation", jobLocation);
+						bed.SetUseState(jobLocation, true);
+						__result =  jobLocation;
+						return false;
+					}
+					break;
+				}
+			}
+
+			return true;
+		}
+	}
+}
